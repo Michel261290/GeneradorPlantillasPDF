65632a5c15f28f38af1c452beb3194a1f86bc735
Michel Pérez <mperez@appwhere.mx>
2018-09-23 17:55:27 -0500

    agregaron cambios para manejar json

--- GeneradorPlantillas.js
+++ GeneradorPlantillas.js
@@ -1,4 +1,5 @@
 var doc;
+
 $(document).ready(function () {
     $("#textoArea").val('InsertarPDF("Hola Mundo", "normal", 10,10);');
     eventoPresionarTecla();
@@ -12,12 +13,10 @@ function eventoPresionarTecla() {
             texto +
             "var string = doc.output('datauristring');" +
             "$('#framePrincipal').attr('src', string).attr('src', string);";
-        console.log(evaluar);
         eval(evaluar);
     } catch (error) {
-
+      console.log(error);
     }
-
 
 }
 
@@ -28,11 +27,38 @@ function InsertarPDF(text, tipoletra, posicionX, posicionY) {
     var margen = parseInt(7);
     altura = parseInt(posicionY);
     doc.setFontType(tipoletra);
-    doc.text(parseInt(margen) + parseInt(posicionX), altura, text)
+    doc.text(parseInt(margen) + parseInt(posicionX), altura, noNulos(text))
 }
 
 function InsertarPagina() {
     doc.addPage();
 }
 
+function noNulos(texto) {
+   if(texto==null || texto==undefined){
+    return "";
+   }
+   return texto.toString();
+}
 
+
+var json = [
+    {
+      "userId": 1,
+      "id": 1,
+      "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+      "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+    },
+    {
+      "userId": 1,
+      "id": 2,
+      "title": "qui est esse",
+      "body": "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla"
+    },
+    {
+      "userId": 1,
+      "id": 3,
+      "title": "ea molestias quasi exercitationem repellat qui ipsa sit aut",
+      "body": "et iusto sed quo iure\nvoluptatem occaecati omnis eligendi aut ad\nvoluptatem doloribus vel accusantium quis pariatur\nmolestiae porro eius odio et labore et velit aut"
+    }
+  ];
